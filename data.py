9c975210af4ef44d689a0cfda57118098e5f187a
Eugenio Herrera-Berg <eugeniohb94@gmail.com>
2023-06-11 18:54:15 -0600

    create basic dataloader (#2)
    * create download_image
    
    * create ImprovedAestheticsDataloader
    
    * refactor image download code
    
    * create functional dataloader prepper
    
    * update readme & improve code readability
    
    * add dataloader usage example

---
+++ data.py
@@ -0,0 +1,67 @@
+import os
+from PIL import Image
+from datasets import load_dataset
+from utils import (
+    overlay_qr,
+    download_image_from_url,
+    generate_random_string,
+)
+    
+
+class ImprovedAestheticsDataloader:
+    image_url_key = "URL"
+    image_path_key = "image_path"
+    qr_image_path_key = "qr_path"
+
+    def __init__(
+        self,
+        split: str = "train",
+        images_folder: str = "images",
+        qr_images_folder: str = 'qr_images',
+    ) -> None:
+        self.images_folder = images_folder
+        if not os.path.exists(images_folder):
+            os.mkdir(images_folder)
+        self.qr_images_folder = qr_images_folder
+        if not os.path.exists(qr_images_folder):
+            os.mkdir(qr_images_folder)
+        self.load_hf_dataset(split)
+
+    def load_hf_dataset(self, split: str) -> None:
+        self.dataset = load_dataset(
+            "ChristophSchuhmann/improved_aesthetics_6.5plus",
+            split=split,
+        )
+
+    def download_image(self, element):
+        image_path = download_image_from_url(element[self.image_url_key], self.images_folder)
+        return image_path
+
+    def create_qr_images(self, element):
+        # create random string
+        url = generate_random_string()
+
+        # load image and create qr path
+        image_path = element[self.image_path_key]
+        print(image_path)
+        image_filename = os.path.basename(image_path)
+        qr_image_path = os.path.join(self.qr_images_folder, image_filename)
+
+        # make overlay and save it
+        image = Image.open(image_path)
+        qr_img = overlay_qr(url=url, image=image, alpha=0.5)
+        qr_img.save(qr_image_path, format="JPEG")
+
+        # create qr column
+        return qr_image_path
+
+    def prepare_data(self):
+        assert self.dataset, "There's no dataset to get images from."
+    
+        print("Downloading images...")
+        dataset = self.dataset.map(lambda element: {self.image_path_key: self.download_image(element)})
+        dataset = dataset.filter(lambda element: element[self.image_path_key] != None)
+        print("Creating QRs...")
+        dataset = dataset.map(lambda element: {self.qr_image_path_key: self.create_qr_images(element)})
+
+        self.dataset = dataset
--- utils.py
+++ utils.py
@@ -1,11 +1,16 @@
-from typing import Union
+import requests
+import os
+import random
+import string
+
+from typing import Union, Optional
 
 import qrcode
 from PIL import Image
+from urllib.parse import urlparse
 
 
 def create_qr(url: str) -> Image.Image:
-
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -34,5 +39,54 @@ def overlay_qr(
 
     # apply overlay
     overlayed_image = Image.blend(image, qr_image, alpha=alpha)
+
+    # convert back into rgb
+    overlayed_image = overlayed_image.convert('RGB')
     
     return overlayed_image
+
+def download_image_from_url(
+    url: str,
+    save_dir: str,
+) -> Optional[str]:
+    try:
+        # get image filename and save path
+        parsed_url = urlparse(url)
+        filename = os.path.basename(parsed_url.path)
+        save_path = os.path.join(save_dir, filename)
+
+        # check if image was already downloaded, otherwise download it
+        if os.path.exists(save_path):
+            print(f"The file {save_path} already exists.")
+        else:
+            response = requests.get(url, stream=True)
+            if response.status_code == 200:
+                with open(save_path, 'wb') as file:
+                    file.write(response.content)
+            else:
+                print(f"Couldn't download image from URL: {url}")
+                return None
+            
+        # crop image to its smallest dimension
+        image = Image.open(save_path)
+        width, height = image.size
+        min_dim = min(width, height)
+        left = (width - min_dim)/2
+        top = (height - min_dim)/2
+        right = (width + min_dim)/2
+        bottom = (height + min_dim)/2
+
+        image = image.crop((left, top, right, bottom))
+
+        # save cropped image
+        image.save(save_path)
+
+        return save_path
+
+    except Exception as e:
+        print(f"Error downloading image from URL: {url}")
+        print(f"Error message: {str(e)}")
+        return None
+    
+def generate_random_string(length: int = 12) -> str:
+    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
