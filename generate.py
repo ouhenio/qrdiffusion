9a2e0b67ec28d36aabf22987fb9ba86901fb6d40
Eugenio Herrera-Berg <eugeniohb94@gmail.com>
2023-06-25 12:40:05 -0400

    add pre-commit (#8)
    * add and apply pre-commit
    
    * add yaml rules

--- generate.py
+++ generate.py
@@ -1,24 +1,23 @@
 import argparse
+
 import torch
-from transformers import AutoTokenizer, CLIPTextModel
 from diffusers import (
     AutoencoderKL,
     ControlNetModel,
-    DDPMScheduler,
     DPMSolverMultistepScheduler,
-    UNet2DConditionModel,
-    StableDiffusionControlNetPipeline,
     StableDiffusionControlNetImg2ImgPipeline,
-    UniPCMultistepScheduler
+    UNet2DConditionModel,
 )
-from utils import create_qr
 from PIL import Image
+from transformers import AutoTokenizer, CLIPTextModel
+
+from utils import create_qr
+
 
 def main(args):
     base_model = args.base_model
     prompt = args.prompt
     negative_prompt = args.negative_prompt
-    qr_prompt = args.qr_prompt
     controlnet_dir = args.controlnet_dir
     cache_dir = args.cache_dir
 
@@ -56,7 +55,9 @@ def main(args):
         cache_dir=cache_dir,
         torch_dtype=torch.float16,
     )
-    pipeline.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config, use_karras=True, algorithm_type="sde-dpmsolver++")
+    pipeline.scheduler = DPMSolverMultistepScheduler.from_config(
+        pipeline.scheduler.config, use_karras=True, algorithm_type="sde-dpmsolver++"
+    )
     pipeline.to("cuda")
 
     qr = create_qr("ouhen.io").resize((768, 768), Image.ANTIALIAS)
@@ -76,14 +77,22 @@ def main(args):
 
     out.images[0].save("output.png")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="QRNet")
-    parser.add_argument('--prompt', type=str, help='Prompt for the model')
-    parser.add_argument('--qr_prompt', type=str, default="ouhen.io")
-    parser.add_argument('--negative_prompt', type=str, help='Negative prompt for the model', default="ugly, disfigured, low quality, blurry, nsfw")
-    parser.add_argument('--base_model', type=str, default="runwayml/stable-diffusion-v1-5")
-    parser.add_argument('--cache_dir', type=str, default=None)
-    parser.add_argument('--controlnet_dir', type=str, default="controlnet")
-    
+    parser.add_argument("--prompt", type=str, help="Prompt for the model")
+    parser.add_argument("--qr_prompt", type=str, default="ouhen.io")
+    parser.add_argument(
+        "--negative_prompt",
+        type=str,
+        help="Negative prompt for the model",
+        default="ugly, disfigured, low quality, blurry, nsfw",
+    )
+    parser.add_argument(
+        "--base_model", type=str, default="runwayml/stable-diffusion-v1-5"
+    )
+    parser.add_argument("--cache_dir", type=str, default=None)
+    parser.add_argument("--controlnet_dir", type=str, default="controlnet")
+
     args = parser.parse_args()
     main(args)
